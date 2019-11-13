9d4b23e62633828e306d8c4a21c2a562c5a26450
Mikael Lemos <mikaellemos@gmail.com>
2019-11-13 20:48:59 -0300

    Add files via upload

---
+++ bash/rmtabwin.sh
@@ -0,0 +1,20 @@
+
+#!/bin/sh
+
+# script por Mikael Lemos
+
+# rm log file
+
+LOGFILE=rm.log
+
+echo "Removendo arquivos não utilizados"
+
+cd \Users\mikael.lemos\Desktop\TABWIN\APAC\script_sh\ftp.datasus.gov.br\dissemin\publicos\SIASUS\200801_\Dados\
+
+rm AMPM*
+rm AMPP*
+rm AMPS*
+rm AMPRO*
+
+echo "Processo concluido"
+
